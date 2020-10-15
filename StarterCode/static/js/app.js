47cc2da42aadd53cd720ea0513dab8a4bfdd9de6
Kumba Ye <kumba@Kumbas-MacBook-Pro.local>
2020-10-14 22:42:57 -0400

    ufo created

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,16 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
 
 // YOUR CODE HERE!
+data.forEach(function(UFO) {
+//   console.log(weatherReport);
+  var row = tbody.append("tr");
+  Object.entries(UFO).forEach(function([key,value]) {
+    // console.log(key, value);
+    // Append a cell to the row for each value
+    // in the weather report object
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
